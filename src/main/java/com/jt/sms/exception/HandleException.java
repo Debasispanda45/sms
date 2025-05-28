ba98578ce50676000f12895ce201bbdd9c5f8fde
captain45 <pandadebasis036@gmail.com>
2025-05-28 20:50:31 +0530

    Refactor exception handling in HandleException to return ProblemDetail for StudentNotFoundException and HttpRequestMethodNotSupportedException

--- src/main/java/com/jt/sms/exception/HandleException.java
+++ src/main/java/com/jt/sms/exception/HandleException.java
@@ -8,6 +8,7 @@ import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -35,9 +36,21 @@ public class HandleException {
     //     return map;
     // }
     @ExceptionHandler(StudentNotFoundException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ProblemDetail handleNoSuchElementException(StudentNotFoundException e) {
-        ProblemDetail problemDetail = ProblemDetail.forStatus(400);
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        problemDetail.setTitle("Not Found");
+        // problemDetail.setDetail(e.getMessage());
+        problemDetail.setProperty("TimeStamp", LocalDateTime.now().toString());
+
+        return problemDetail;
+    }
+    
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ProblemDetail handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.METHOD_NOT_ALLOWED, e.getMessage());
+        problemDetail.setTitle("Method Not Allowed");
+        problemDetail.setProperty("TimeStamp", LocalDateTime.now().toString());
+
         return problemDetail;
     }
 }
