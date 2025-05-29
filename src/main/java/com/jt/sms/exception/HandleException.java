1b47293e3b9551a4932678db735fd2d8c1bec380
captain45 <pandadebasis036@gmail.com>
2025-05-29 18:22:40 +0530

    Remove unused imports in HandleException and StudentController for cleaner code

--- src/main/java/com/jt/sms/exception/HandleException.java
+++ src/main/java/com/jt/sms/exception/HandleException.java
@@ -1,16 +1,10 @@
 package com.jt.sms.exception;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
