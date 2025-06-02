383105a765e2cf49e96942dfa710fbf3718e8806
captain45 <pandadebasis036@gmail.com>
2025-06-02 19:10:53 +0530

    Add springdoc-openapi dependency and enhance Student entity with email, fees, and phone number fields

--- src/main/java/com/jt/sms/entity/Student.java
+++ src/main/java/com/jt/sms/entity/Student.java
@@ -4,7 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,20 +23,35 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Student {                                                                                        
+public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String studentid;
 
     @Positive
+    @Min(100)
     private int roll;
 
+    @NotEmpty
+    @NotNull
+    @NotBlank
     private String nam;
-    
+
+    @NotEmpty
+    @NotNull
+    @NotBlank
+    @Email(message = "Email should be valid")
     private String email;
 
+    @PositiveOrZero
+    // @Digits(integer = 10, fraction = 2, message = "Fees should be a positive
+    // number with up to two decimal places")
     private double fees;
 
+    @NotEmpty
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^[6-9][0-9]{9}$", message = "Phone number should be a valid Indian mobile number")
     private String phoneNumber;
-    
+
 }
